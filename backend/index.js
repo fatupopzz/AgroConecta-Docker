70ba0f202cb0d4bec51b6082041ecdeaf542323e
Juan Rivas <riv24856@uvg.edu.gt>
2026-04-18 17:57:25 -0600

    CRUD Agricultor

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const productRoutes = require("./src/routes/productRoutes");
 const categoryRoutes = require("./src/routes/categoryRoutes");
 const authRoutes = require("./src/routes/authRoutes");
 const verifyToken = require("./src/middleware/authMiddleware");
+const agricultorRoutes = require("./src/routes/agricultorRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -24,7 +25,7 @@ app.get("/api/protected", verifyToken, (req, res) => {
 
 app.use("/api/products", productRoutes);
 app.use("/api/categories", categoryRoutes);
-
+app.use("/api/agricultores", agricultorRoutes);
 app.get("/", (req, res) => {
   res.json({ status: "AgroConecta Backend corriendo", version: "1.0.0" });
 });
