f1ad384255294f15c86d48a01a4f5c98882934de
fatitaprogramando <nav24044@uvg.edu.gt>
2026-05-15 18:50:43 -0600

    fix(KAN-53): corregir duplicado en inventoryRoutes.js

--- backend/src/routes/inventoryRoutes.js
+++ backend/src/routes/inventoryRoutes.js
@@ -5,11 +5,4 @@ const { createInventory } = require("../controllers/inventoryController");
 
 router.post("/", verifyToken, createInventory);
 
-module.exports = router;const express = require("express");
-const router = express.Router();
-const verifyToken = require("../middleware/authMiddleware");
-const { createInventory } = require("../controllers/inventoryController");
-
-router.post("/", verifyToken, createInventory);
-
 module.exports = router;
